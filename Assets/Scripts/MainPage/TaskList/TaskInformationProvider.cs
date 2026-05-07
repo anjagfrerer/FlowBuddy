3aa465de64063c6c5eab208a6ec21b8393ab0f2c
Talveri <talveri124@gmail.com>
2026-05-07 12:42:09 +0200

    added tasklist

---
+++ Assets/Scripts/MainPage/TaskList/TaskInformationProvider.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+public class TaskInformationProvider : MonoBehaviour
+{
+    public TMP_Text SubjectLabel;
+    public TMP_Text titleLabel;
+    public TMP_Text DescriptionText;
+    
+    public TMP_Text DueDate;
+}

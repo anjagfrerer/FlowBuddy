fb3f34f4fda548ffe2d7a3c47d2e37ebc77e97be
Amelia Maier <ame.m2805@gmail.com>
2026-06-03 15:07:49 +0200

    Added coins field to user. Coins are incremented by 10 each time they tick a task in a learning package.

---
+++ Assets/Scripts/Class/ShopItem.cs
@@ -0,0 +1,8 @@
+[System.Serializable]
+
+public class ShopItem
+{
+    private string name;
+    private string image;
+    private int price;
+}
--- Assets/Scripts/MainPage/TickableTaskItem.cs
+++ Assets/Scripts/MainPage/TickableTaskItem.cs
@@ -60,6 +60,12 @@ public class TickableTaskItem : MonoBehaviour
         if (currentTask != null)
         {
             currentTask.isDone = isOn;
+
+            if (isOn)
+            {
+                DataManager.Instance.appData.user.coins += 10;
+            }
+
             DataManager.Instance.SaveData(); 
         }
 
